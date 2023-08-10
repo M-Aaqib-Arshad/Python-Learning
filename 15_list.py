0cec2562a34419a31be5a6ba8220a56b38ddd2e1
Muhammad Aaqib Arshad <muhammad.aaqib.arshad@gmail.com>
2023-08-10 19:47:17 +0500

    Addition in tuples and lists

--- 15_list.py
+++ 15_list.py
@@ -3,21 +3,30 @@ lst = [2,5,1,6,1,2,4,5,"Aaqib"]
 # In python, it can not change the string and in slicing python make a new string 
 # but in list you can modify and change it.
 
-print(type(lst)) 
 print(lst)
+print(type(lst)) 
 
 # Methods
 
 lst.remove("Aaqib")
+print("After removing function",lst) 
 # print(lst.count(1))
 
 # lst.sort()
-lst.pop()  # you can also give the index of item like lst.pop(3)
+lst.pop()  #del the random one, and you can also give the index of item like lst.pop(3)
+print("After pop function",lst) 
+
+print("For we ",lst.pop())
+
 lst.append(7)
+print("After append function",lst) 
+
 # lst.clear() Output empty list
 lst.extend([8,9,0]) # you can also add tuple in the list
+print("After extend function",lst) 
 
 lst.insert(8,10)
+print("After insert function",lst) 
 
 print(lst.index(9))
 print(lst)
